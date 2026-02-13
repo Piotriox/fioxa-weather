7677b75ec261739e77f213e6392e8a9436490fcf
Piotriox <ardaege2019@gmail.com>
2026-02-13 18:43:15 +0300

    Some little tests.

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    <h1>Fioxa Weather</h1>
     </>
   )
 }
